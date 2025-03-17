dd161459b8ef80a8cb966c9f36c3c4511c1de2aa
Aki Tuomi <aki.tuomi@open-xchange.com>
2025-03-17 13:27:54 +0200

    Add build utility

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -eux
+
+VERSION=${VERSION-2.4.1}
+DOVECOT_REPO_URL=${DOVECOT_REPO_URL-https://github.com/dovecot/core}
+PIGEONHOLE_REPO_URL=${PIGEONHOLE_REPO_URL-https://github.com/dovecot/pigeonhole}
+DOVECOT_BRANCH=${DOVECOT_BRANCH-$VERSION}
+PIGEONHOLE_BRANCH=${PIGEONHOLE_BRANCH-$VERSION}
+
+amd64_CFLAGS="-g -O2 -fno-omit-frame-pointer -mno-omit-leaf-frame-pointer -flto=auto -ffat-lto-objects -fstack-protector-strong -fstack-clash-protection -Wformat -Werror=format-security -fcf-protection=full"
+arm64_CFLAGS="-g -O2 -fno-omit-frame-pointer -mno-omit-leaf-frame-pointer -flto=auto -ffat-lto-objects -fstack-protector-strong -fstack-clash-protection -Wformat -Werror=format-security"
+
+amd64_LDFLAGS="-Wl,-Bsymbolic-functions -flto=auto -ffat-lto-objects"
+arm64_LDFLAGS="-Wl,-Bsymbolic-functions -flto=auto -ffat-lto-objects"
+
+for PLATFORM in amd64 arm64; do
+	for stage in "-build" "-root" "-dev" ""; do
+		PLATFORM_CFLAGS="${PLATFORM}_CFLAGS"
+		PLATFORM_LDFLAGS="${PLATFORM}_LDFLAGS"
+		docker buildx build --platform $PLATFORM \
+			--build-arg CFLAGS="${!PLATFORM_CFLAGS}" --build-arg LDFLAGS="${!PLATFORM_LDFLAGS}" \
+			--build-arg DOVECOT_REPO_URL=$DOVECOT_REPO_URL --build-arg DOVECOT_BRANCH=$DOVECOT_BRANCH \
+			--build-arg PIGEONHOLE_REPO_URL=$PIGEONHOLE_REPO_URL --build-arg PIGEONHOLE_BRANCH=$DOVECOT_BRANCH --target production$stage --tag dovecot/dovecot:$VERSION${stage}-$PLATFORM $VERSION
+	done
+done
